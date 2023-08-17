a3d4d0a54fa2b33995301684c2269eb50db0799e
Mike Farad <mike18farad@gmail.com>
2023-08-17 12:15:21 +0300

    feat: create health check client

---
+++ payments-service/pkg/clients/client.go
@@ -0,0 +1,42 @@
+package payments
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/Mik3y-F/order-management-system/payments/api/generated"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type HealthCheckRequest = pb.HealthCheckRequest
+type HealthCheckResponse = pb.HealthCheckResponse
+
+type OrdersClient interface {
+	HealthCheck(ctx context.Context, product *HealthCheckRequest) (*HealthCheckResponse, error)
+}
+
+type GrpcOrderClient struct {
+	conn   *grpc.ClientConn
+	client pb.OrdersClient
+}
+
+func NewGrpcOrderClient(conn *grpc.ClientConn) *GrpcOrderClient {
+
+	client := pb.NewOrdersClient(conn)
+
+	return &GrpcOrderClient{
+		conn:   conn,
+		client: client,
+	}
+}
+
+func ConnectToOrderService(address string) (*grpc.ClientConn, error) {
+
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+
+	return conn, nil
+}
