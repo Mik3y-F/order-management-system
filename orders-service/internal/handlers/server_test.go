a6b9dae7a2e1a5da9928dacd2befe7a16ee54855
Mike Farad <mike18farad@gmail.com>
2023-08-20 03:44:08 +0300

    fix: fix test race condition

--- orders-service/internal/handlers/server_test.go
+++ orders-service/internal/handlers/server_test.go
@@ -3,6 +3,7 @@ package handlers_test
 import (
 	"fmt"
 	"net"
+	"sync"
 	"testing"
 	"time"
 
@@ -70,7 +71,11 @@ func TestGRPCServer_Run(t *testing.T) {
 }
 
 // Helper function to get a free port
-func getFreePort(t testing.TB) string {
+func getFreePort(t *testing.T) string {
+	m := &sync.Mutex{}
+	m.Lock()
+	defer m.Unlock()
+
 	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
 	if err != nil {
 		t.Fatalf("error resolving tcp address: %v", err)
